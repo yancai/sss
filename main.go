873ae8747e6d4d13d4f935551d193eaf1c403609
yancai <yancai915@gmail.com>
2018-08-30 23:17:36 +0800

    粗糙的实现了ssh登录，需要完善命令行输入账号密码

--- main.go
+++ main.go
@@ -10,13 +10,14 @@ import (
 	"syscall"
 
 	"github.com/urfave/cli"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 const VERSION string = "1.0.0"
 
 func main() {
 	initCmdArgs()
-	executeCmd("ping", "127.0.0.1")
 }
 
 /**
@@ -66,6 +67,12 @@ func initCmdArgs() {
 			Usage:   "show session list",
 			Action:  showSessionList,
 		},
+		{
+			Name:    "connect",
+			Aliases: []string{"c"},
+			Usage:   "connect to ssh",
+			Action:  runSSH,
+		},
 	}
 
 	app.Run(os.Args)
@@ -105,4 +112,60 @@ func executeCmd(name string, arg ...string) {
 		log.Println("execute error")
 		fmt.Printf("execute error: %v", exeErr)
 	}
+}
+
+func runSSH(c *cli.Context) error {
+	//var hostKey ssh.PublicKey
+	// Create client config
+	config := &ssh.ClientConfig{
+		User: "username",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("123456"),
+		},
+		//HostKeyCallback: ssh.FixedHostKey(hostKey),
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	// Connect to ssh server
+	conn, err := ssh.Dial("tcp", "127.0.0.1:22", config)
+	if err != nil {
+		log.Fatal("unable to connect: ", err)
+	}
+	defer conn.Close()
+	// Create a session
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Fatal("unable to create session: ", err)
+	}
+
+	fd := int(os.Stdin.Fd())
+	oldState, err := terminal.MakeRaw(fd)
+	if err != nil {
+		log.Fatal("make raw error: ", err)
+	}
+	defer terminal.Restore(fd, oldState)
+
+	session.Stdin = os.Stdin
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+
+	tw, th, err := terminal.GetSize(fd)
+	if err != nil {
+		log.Fatal("get size error: ", err)
+	}
+
+	defer session.Close()
+	// Set up terminal modes
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          1,
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	// Request pseudo terminal
+	if err := session.RequestPty("xterm", th, tw, modes); err != nil {
+		log.Fatal("request for pseudo terminal failed: ", err)
+	}
+
+	session.Run("bash")
+	return nil
 }
